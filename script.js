f0b95522b355114b772d640be0f9922c6337ff9d
Aidil M <78016014+Aidil521@users.noreply.github.com>
2024-02-11 23:22:12 +0700

    buat file javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw14rAxgvmfIMXuvWkYW_LNvzcC1r_r_PrvY3kAzMuj5HSyRhHT-MqUaQdro91nbba4/exec';
+const form = document.forms['kontak-saya'];
+
+const btnSubmit = document.querySelector('.btn-submit');
+const btnLoading = document.querySelector('.btn-loading');
+const Myalert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    btnSubmit.classList.toggle('d-none');
+    btnLoading.classList.toggle('d-none');
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            btnSubmit.classList.toggle('d-none');
+            btnLoading.classList.toggle('d-none');
+            Myalert.classList.toggle('d-none');
+            form.reset();
+            console.log('Success!', response);
+        })
+        .catch(error => console.error('Error!', error.message))
+});
